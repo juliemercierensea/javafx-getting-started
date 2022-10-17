746d60ef5656c98ebd1016884b6c255586ec4724
juliemercierensea <j.mercier@bbox.fr>
2022-10-17 12:00:27 +0200

    MyHeader in progress

---
+++ src/main/java/org/openjfx/MyHeader.java
@@ -0,0 +1,21 @@
+package org.openjfx;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
+public class MyHeader extends VBox {
+    public MyHeader() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("my_header.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+//ajouter fonction pour ajouter des valeurs aléatoire avec le bouton
+}
